bd0c04ce35c17bc623752c4eb7a5ba97094a7c19
Anurag Negi <anurag.negi862@gmail.com>
2025-07-27 00:15:47 +0530

    struct for withdraw completed

--- programs/automated-market-makers/src/instructions/withdraw.rs
+++ programs/automated-market-makers/src/instructions/withdraw.rs
@@ -20,5 +20,52 @@ pub struct Withdraw<'info>{
         has_one=mint_a,
         has_one=mint_b
     )]
+    pub config:Account<'info,Config>,
+
+    #[account(
+        mut,
+        seeds=[b"lp",config.key().as_ref()],
+        bump=config.lp_bump
+    )]
+    pub mint_lp:Account<'info,Mint>,
+
+    #[account(
+        mut,
+        associated_token::mint=mint_a,
+        associated_token::authority=config
+    )]
+    pub vault_a:Account<'info,TokenAccount>,
+
+    #[account(
+        mut,
+        associated_token::mint=mint_b,
+        associated_token::authority=config
+    )]
+    pub vault_b:Account<'info,TokenAccount>,
+
+    #[account(
+        mut,
+        associated_token::mint=mint_a,
+        associated_token::authority=signer
+    )]
+    pub user_token_account_a:Account<'info,TokenAccount>,
+
+    #[account(
+        mut,
+        associated_token::mint=mint_b,
+        associated_token::authority=signer
+    )]
+    pub user_token_account_b:Account<'info,TokenAccount>,
+
+    #[account(
+        mut,
+        associated_token::mint=mint_lp,
+        associated_token::authority=signer
+    )]
+    pub user_token_account_lp:Account<'info,TokenAccount>,
+
+    pub associated_token_program:Program<'info,AssociatedToken>,
+    pub token_program:Program<'info,Token>,
+    pub system_program:Program<'info,Program>
 
 }
