59d887bad31f7d18c7006b35430f6b64e65031b7
Anurag Negi <anurag.negi862@gmail.com>
2025-07-28 04:22:45 +0530

    withdraw tokens and lptokens

--- programs/automated-market-makers/src/instructions/withdraw.rs
+++ programs/automated-market-makers/src/instructions/withdraw.rs
@@ -69,3 +69,80 @@ pub struct Withdraw<'info>{
     pub system_program:Program<'info,Program>
 
 }
+
+impl <'info> Withdraw <'info>{
+    pub fn withdraw(&mut self,amount:u64,min_a:u64,min_b:u64)->Result<()>{
+        require!(amount!=0,AmmError::InvalidAmount);
+        let (a , b)=match self.mint_lp.supply==0
+        && self.vault_a.amount==0
+        && self.vault_b.amount==0
+        {
+            true=>(min_a,min_b),
+            false=>{
+                let amount=ConstantProduct::xy_deposit_amounts_from_l(
+                    self.vault_a.amount,
+                    self.vault_b.amount,
+                    self.mint_lp.supply,
+                    amount,
+                    6
+                ).unwrap();
+                (amount.x,amount.y)
+            }
+        };
+        require!(x>=min_a && y>=min_y,AmmError::SlippageExceeded);
+        self.burn_lp(amount);
+        self.withdraw_token_a(a);
+        self.withdraw_token_b(b);
+        Ok(())
+    }
+
+    pub fn burn_lp(&mut self,amount:u64)->Result<()>{
+        let cpi_program=self.token_program.to_account_info();
+        let cpi_accounts=Burn{
+            mint:self.mint_lp.to_account_info(),
+            from:self.user_token_account_lp.to_account_info(),
+            authority:self.user.to_account_info()
+        };
+        let signer_seeds=&[&[&[u8]]]=&[&[
+            b"config",
+            &[self.config.config_bump],
+        ]];
+
+        let cpi_ctx=CpiContext::new_with_signer(cpi_program,cpi_accounts,signer_seeds);
+        burn(cpi_ctx,amount);
+        Ok(())
+    }
+
+    pub fn withdraw_token_a(&mut self,amount:u64)->Result<()>{
+        let cpi_program=self.token_program.to_account_info();
+        let cpi_accounts=TransferChecked{
+            from:self.vault_a.to_account_info(),
+            to:self.user_token_account_a.to_account_info(),
+            mint:self.mint_a.to_account_info(),
+            authority:self.config.to_account_info()
+        };
+        let signer_seeds:&[&[&[u8]]]=&[&[
+            b"config",
+            &[self.config.config_bump]
+        ]];
+        let cpi_ctx=CpiContext::new_with_signer(cpi_program,cpi_accounts,signer_seeds);
+        transfer_checked(cpi_ctx,amount,decimals);
+        Ok(())
+    }
+    pub fn withdraw_token_b(&mut self,amount:u64)->Result<()>{
+        let cpi_program=self.token_program.to_account_info();
+        let cpi_accounts=TransferChecked{
+            from:self.vault_b.to_account_info(),
+            to:self.user_token_account_b.to_account_info(),
+            mint:self.mint_b.to_account_info(),
+            authority:self.config.to_account_info()
+        };
+        let signer_seeds:&[&[&[u8]]]=&[&[
+            b"config",
+            self.config.config_bump
+        ]];
+        let cpi_ctx=CpiContext::new_with_signer(cpi_account,cpi_accounts,signer_seeds);
+        transfer_checked(cpi_ctx,amount,decimals);
+        Ok(())
+    }
+}
